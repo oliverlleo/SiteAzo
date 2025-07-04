865d24d12892e733d2d3daed7baf1115f285e9c8
Leonardo Rodrigues de Oliveira <85530968+oliverlleo@users.noreply.github.com>
2025-07-04 14:55:52 -0300

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -4,11 +4,14 @@ import tailwindcss from '@tailwindcss/vite'
 import path from 'path'
 
 // https://vite.dev/config/
-export default defineConfig({
-  plugins: [react(),tailwindcss()],
-  resolve: {
-    alias: {
-      "@": path.resolve(__dirname, "./src"),
-    },
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {},
   },
-})
+  plugins: [],
+}
